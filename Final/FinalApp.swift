2d06ff97421141fb110171a18566ef3b68392757
nexus <nexusgood93new@gmail.com>
2025-11-22 15:36:31 +0800

    Initial Commit

---
+++ Final/FinalApp.swift
@@ -0,0 +1,17 @@
+//
+//  FinalApp.swift
+//  Final
+//
+//  Created by 陳詠平 on 2025/11/22.
+//
+
+import SwiftUI
+
+@main
+struct FinalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
